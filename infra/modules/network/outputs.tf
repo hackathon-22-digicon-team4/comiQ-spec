482e42fbfee2a0aa2e4472827ea1c33c4c9d9636
kyosu-1 <sho013039@gmail.com>
2022-09-26 21:08:19 +0900

    fix

--- infra/modules/network/outputs.tf
+++ infra/modules/network/outputs.tf
@@ -3,3 +3,7 @@ output "private_sub_1a" { value = aws_subnet.private_1a }
 output "public_sub_1a" { value = aws_subnet.public_1a }
 output "private_sub_1c" { value = aws_subnet.private_1c }
 output "public_sub_1c" { value = aws_subnet.public_1c }
+output "sg_for_alb" { value = aws_security_group.for_alb }
+output "sg_for_ecs" { value = aws_security_group.for_ecs }
+output "sg_for_rds" { value = aws_security_group.for_rds }
+output "sg_for_ssh" { value = aws_security_group.for_ssh }
