bbfa4715452be2a1d7e093196e5851c61598d152
kyosu-1 <sho013039@gmail.com>
2022-09-26 21:14:28 +0900

    add alb

---
+++ infra/modules/routing/outputs.tf
@@ -0,0 +1,2 @@
+output "tg" { value = aws_lb_target_group.tg }
+output "aws_lb_listener" { value = aws_lb_listener.https }
---
+++ infra/modules/routing/variables.tf
@@ -0,0 +1,6 @@
+variable "sg_for_alb" {}
+variable "public_1a" {}
+variable "public_1c" {}
+variable "vpc" {}
+variable "stage" {}
+variable "project" {}
