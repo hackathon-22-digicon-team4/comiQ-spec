09cc4e0bb6f30cbc333eccd80d8ff8f4956a8b74
kyosu-1 <sho013039@gmail.com>
2022-09-26 19:34:17 +0900

    apply network

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -22,3 +22,16 @@ terraform {
     }
   }
 }
+
+module "production_network" {
+  source = "../../modules/network"
+  project = var.project
+  stage = var.stage
+  cidr_block_vpc = "10.0.0.0/16"
+  cidr_block_public_1a = "10.0.10.0/24"
+  cidr_block_public_1c = "10.0.20.0/24"
+  cidr_block_private_1a = "10.0.30.0/24"
+  cidr_block_private_1c = "10.0.40.0/24"
+  az_1a = "ap-northeast-1a"
+  az_1c = "ap-northeast-1c"
+}
