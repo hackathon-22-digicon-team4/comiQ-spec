9c1189746d92671a3f66a5f923def34bc23bee0b
kyosu-1 <sho013039@gmail.com>
2022-09-27 07:00:43 +0900

    fix name

--- infra/modules/cd/main.tf
+++ infra/modules/cd/main.tf
@@ -74,7 +74,7 @@ data "aws_iam_policy_document" "github_actions_assume_role_policy_for_server" {
 }
 
 resource "aws_iam_policy" "deploy_for_server" {
-  name = "comiq-frontend.deploy"
+  name = "comiq-server.deploy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
