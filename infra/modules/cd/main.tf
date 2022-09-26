b3eb2ffdd5550bfa8d17dc7e03c1809f06e3206c
kyosu-1 <sho013039@gmail.com>
2022-09-27 06:56:46 +0900

    add server cd module

--- infra/modules/cd/main.tf
+++ infra/modules/cd/main.tf
@@ -54,3 +54,52 @@ resource "aws_iam_role" "github_actions" {
     aws_iam_policy.deploy.arn,
   ]
 }
+
+data "aws_iam_policy_document" "github_actions_assume_role_policy_for_server" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["${data.terraform_remote_state.common_resource.outputs.github_actions_oidc_arn}"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:hackathon-22-digicon-team4/comiQ-server:*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "deploy_for_server" {
+  name = "comiq-frontend.deploy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Sid" : "ECR",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart",
+          "ecr:GetAuthorizationToken",
+        ],
+        "Resource" : "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "github_actions_for_server" {
+  name = "comiq-server-github-actions"
+
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy_for_server.json
+  managed_policy_arns = [
+    aws_iam_policy.deploy_for_server.arn,
+  ]
+}
