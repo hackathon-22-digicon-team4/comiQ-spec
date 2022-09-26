711a8e16736c4f93de1346ff5457805d38955893
kyosu-1 <sho013039@gmail.com>
2022-09-26 19:53:13 +0900

    add sg

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -7,3 +7,7 @@ variable "cidr_block_private_1a" {}
 variable "cidr_block_private_1c" {}
 variable "az_1a" {}
 variable "az_1c" {}
+output "sg_for_alb" { value = aws_security_group.for_alb }
+output "sg_for_ecs" { value = aws_security_group.for_ecs }
+output "sg_for_rds" { value = aws_security_group.for_rds }
+output "sg_for_ssh" { value = aws_security_group.for_ssh }
